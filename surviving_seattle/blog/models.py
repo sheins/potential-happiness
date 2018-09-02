1a3c45180889c7f5c0a2bfa6d5f7c3008958eff6
Sara Heins <saraheins@Saras-MacBook-Air.local>
2018-09-02 15:52:36 -0700

    Migrations for blog app, plus added blog post page

--- surviving_seattle/blog/models.py
+++ surviving_seattle/blog/models.py
@@ -1,11 +1,24 @@
+from django.db import models
 from wagtail.core.models import Page
 from wagtail.core.fields import RichTextField
 from wagtail.admin.edit_handlers import FieldPanel
 
 
 class BlogIndexPage(Page):
-    body = RichTextField(blank=True)
+    body = RichTextField(blank=True)
 
-    content_panels = Page.content_panels + [
-        FieldPanel('body') 
-    ]
+    content_panels = Page.content_panels + [
+        FieldPanel('body')
+    ]
+
+
+class BlogPage(Page):
+    date = models.DateField("Post date")
+    summary = models.CharField(max_length=250)
+    body = RichTextField(blank=True)
+
+    content_panels = Page.content_panels + [
+        FieldPanel('date'),
+        FieldPanel('summary'),
+        FieldPanel('body', classname="full"),
+    ]
